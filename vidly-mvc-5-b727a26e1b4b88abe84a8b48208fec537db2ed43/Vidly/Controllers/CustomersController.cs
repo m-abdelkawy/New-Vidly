cd5a08801d163a8d93a8bd30c913f238369bd121
M.Abdelkawy <engineer_m.abdelkawy@yahoo.com>
2018-10-01 02:04:15 +0200

    new

--- vidly-mvc-5-b727a26e1b4b88abe84a8b48208fec537db2ed43/Vidly/Controllers/CustomersController.cs
+++ vidly-mvc-5-b727a26e1b4b88abe84a8b48208fec537db2ed43/Vidly/Controllers/CustomersController.cs
@@ -7,17 +7,27 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-
+        ApplicationDbContext _ctx;
+        public CustomersController()
+        {
+            _ctx = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+        }
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+            IEnumerable<Customer> customers = _ctx.Customers;
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            Customer customer = _ctx.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
